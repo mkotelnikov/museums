311c67046270068bb6946efd7c901e46413eaad7
Mikhail Kotelnikov <mikhail.kotelnikov@gmail.com>
2016-08-11 19:19:24 +0200

    Fix data import

--- bin/museums.import.sh
+++ bin/museums.import.sh
@@ -6,11 +6,17 @@ dir=`pwd`
 lang=french
 baseUrl="http://localhost:9876/museums/$lang"
 
+# Drop "museums" collection
+curl -H "Content-Type: application/json" -X DELETE "$baseUrl/museums"
+
 # Create "museums" collection
 curl -H "Content-Type: application/json" -X PUT "$baseUrl/museums"
 
 # Save data 
 curl -H "Content-Type: application/json" -X PUT --data-binary "@$dir/museums.geo.json" "$baseUrl/museums/data"
+
+# Drop index
+curl -H "Content-Type: application/json" -X DELETE "$baseUrl/museums/index/q"
 
 # Create index
 curl -H "Content-Type: application/json" -X PUT "$baseUrl/museums/index/q" -d '{"properties.name":2,"properties.description":1,"properties.tags":2}'
